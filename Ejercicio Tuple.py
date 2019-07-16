5e52b3f431472efe358c1565a8bdad95db2f972e
Jusebamo_1 <jusebamo1@hotmail.com>
2019-07-16 17:47:28 -0500

    Clases

---
+++ Ejercicio Tuple.py
@@ -0,0 +1,31 @@
+
+semana = [martes,miercoles,jueves,viernes,sabado,domingo]
+
+def Dia():
+    print(i)
+    if i == 32 :
+        i = 0
+        i +=1
+    else:
+        
+
+def Año(): 
+    año = int(input("Introduzca un año"))
+
+def mes():
+    mes = int(input("Introduzca un mes"))
+
+def Menu():
+    print("--------------------------------")
+    print(     "Bienvenido al sistema")
+    print("-------------------------------")
+    print("")
+
+ dia = 0
+ mes = 0
+ año = 0
+while True:
+    Menu()
+    
+    Dia()
+    break
---
+++ calendario.py
@@ -0,0 +1,30 @@
+#semana = ("lunes","domingo","sabado","viernes","jueves","miercoles","martes") 
+semanaReal = ("lunes","martes","miercoles","jueves","viernes","sabado","domingo")
+semana = []
+
+n = 0
+#busca el dia de la semana...
+def BuscarDiadelaSemana(n):
+    if (n > 7):
+        print(semana[(n%7)-1])    
+    else:
+        print(semana[n-1]) 
+
+#Orden del dia ingresado...
+def OrdenDia(dia):
+    print(semanaReal.index(dia))
+    x = semanaReal.index(dia)
+    y = x-7
+    for i in range(x, y , -1):
+        print(semanaReal[i])
+        semana3 = semana.append()
+
+        
+
+    
+
+while True :
+   D = OrdenDia(input("Dia de la semana:"))
+   B = BuscarDiadelaSemana(int(input("# de Dias que quiere retroceder:")))
+
+   break 
