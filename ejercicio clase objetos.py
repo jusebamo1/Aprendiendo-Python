818d595192b6d6d2ab4e227b874e2b22dbe28c04
Jusebamo_1 <jusebamo1@hotmail.com>
2019-07-30 16:26:11 -0500

    Ejercicio Clase Objetos Modificado

--- ejercicio clase objetos.py
+++ ejercicio clase objetos.py
@@ -3,11 +3,17 @@ import time as t
 #Ejercicio objetos
 
 class AerosolPintura():
+	#Constructor
+	def __init__(self,color,level,colorEnvase):
+		self.colorDelaPintura=color
+		self.__nivelGas = level
+		self.colorEnvase = colorEnvase
+		
+			
+
 	#Propiedades
 
 	forma = "cilindrica"
-	colorDelaPintura= "Amarillo"
-	colorEnvase = "Amarillo y negro"
 	material = "Metalico"
 	reciclable = "Si"
 	estadoOprimido = False
@@ -22,28 +28,54 @@ class AerosolPintura():
 			print("Mezclado terminado.")
 			break
 
-	def ExpulsarPintura(self, nivelGas):
+	def ExpulsarPintura(self,tiempo):
 
 		print("Expulsando Pintura...")
-		
-		for i in range (nivelGas):	
-			t.sleep(1)
-			print("....")
-			print("Expulsion exitosa")
+		self.__nivelGas -= 1	
+		t.sleep(tiempo)
+		print("....")
+		print("Expulsion exitosa")
 			
 		
 
-	def oprimir(self, presionado):
-		if presionado:
-			self.ExpulsarPintura (self.100)
-			#pass
+	def oprimir(self, presionado, tiempo):
+		if presionado and self.__nivelGas > 0:
+			self.ExpulsarPintura(tiempo)
+		elif presionado and self.__nivelGas == 0:
+			print("Nivel de gas acabado.")
+
+	def mostarNivel(self):
+		return self.__nivelGas
+
 
 
 	
 
-PinturaNegra = AerosolPintura()
-PinturaNegra.oprimir(True);
+PinturaNegra = AerosolPintura("Negro",100,"Blanco")
+PinturaNaranja = AerosolPintura("Naranja",50,"Naranja")
+PinturaNegra.oprimir(True,2)
 
+print(PinturaNegra.colorDelaPintura)
 
+PinturaNegra.oprimir(True,2)
+print(PinturaNegra.mostarNivel())
 
+PinturaNegra.__nivelGas=100
+PinturaNegra.oprimir(True,2)
+print(PinturaNegra.mostarNivel())
 
+PinturaNegra.Mezclar(5)
+print(PinturaNegra.mostarNivel())
+
+######
+print(PinturaNaranja.colorDelaPintura)
+
+PinturaNaranja.oprimir(True,2)
+print(PinturaNaranja.mostarNivel())
+
+PinturaNaranja.__nivelGas=100
+PinturaNaranja.oprimir(True,2)
+print(PinturaNaranja.mostarNivel())
+
+PinturaNaranja.Mezclar(5)
+print(PinturaNaranja.mostarNivel())
