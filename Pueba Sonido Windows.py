6962f2b362db61afce3a22f598d3d84b0ca5af59
Jusebamo_1 <jusebamo1@hotmail.com>
2019-08-02 17:22:22 -0500

    Prueba audio Proyecto parlante

--- Pueba Sonido Windows.py
+++ Pueba Sonido Windows.py
@@ -3,26 +3,34 @@ import time
 
 class parlante():
     #Constructor
-    def __init__(self,color,marca, status):
+    def __init__(self,color,marca, status,nivel,Vnivel):
         self.colorParlante= color
         self.marcaP = marca
         self.isEncendido = status
+        self.bateria = nivel
+        self.volumen = Vnivel
     #Propiedades
     material = "plastico"
     estado = False
     
 
-   #Metodos||Acciones
+   #Metodos||Acciones:
+##############---- MUSICA SISTEMA ----------------#############
+
+    def __MusicEncendido(self):
+        Beep(500,200)
+        Beep(800,200)
+
+    def __MusicApagado(self):
+        Beep(200,100)
+        Beep(700,200)
+
+##################------CANCIONES-------##########################
 
     def __TocarMusica1(self):
-     
-       #Beep(frequency, duration) Frequency in Hertz, duration in ms
        ###################################################
        # Mario Bros Theme ( Beep Music )##################
-       # Address: https://www.autoitscript.com/forum/topic/40848-beep-music-mario-bros-theme/
-       ###################################################
-       ###################### by J0keR  ##################
-       ###################################################
+       # Address: https://www.autoitscript.com/forum/topic/40848-Beep-music-mario-bros-theme/
         Beep(480,200)
         Beep(1568,200)
         Beep(1568,200)
@@ -368,6 +376,222 @@ class parlante():
         Beep(523, 125)
         print("Song finished")
         
+    def __TocarMusica3(self):
+        Beep (659, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (622,120)  #  Treble D#
+        time.sleep(0.120)
+
+        Beep (659, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (622, 120)  #  Treble D#
+        time.sleep(0.120)
+        Beep (659, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (494, 120)  #  Treble B
+        time.sleep(0.120)
+        Beep (587, 120)  #  Treble D
+        time.sleep(0.120)
+        Beep (523, 120)  #  Treble C
+        time.sleep(0.120)
+
+        Beep (440, 120)  #  Treble A
+        time.sleep(0.140)
+        Beep (262, 120)  #  Middle C
+        time.sleep(0.120)
+        Beep (330, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (440, 120)  #  Treble A
+        time.sleep(0.120)
+
+        Beep (494, 120)  #  Treble B
+        time.sleep(0.140)
+        Beep (330, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (415, 120)  #  Treble G#
+        time.sleep(0.120)
+        Beep (494, 120)  #  Treble B
+        time.sleep(0.120)
+
+        Beep (523, 120)  #  Treble C
+        time.sleep(0.140)
+        Beep (330, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (659, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (622, 120)  #  Treble D#
+        time.sleep(0.120)
+
+        Beep (659, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (622, 120)  #  Treble D#
+        time.sleep(0.120)
+        Beep (659, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (494, 120)  #  Treble B
+        time.sleep(0.120)
+        Beep (587, 120)  #  Treble D
+        time.sleep(0.120)
+        Beep (523, 120)  #  Treble C
+        time.sleep(0.120)
+
+        Beep (440, 120)  #  Treble A
+        time.sleep(0.140)
+        Beep (262, 120)  #  Middle C
+        time.sleep(0.120)
+        Beep (330, 120)  #  Treble E
+        time.sleep(0.120)
+        Beep (440, 120)  #  Treble A
+        time.sleep(0.120)
+
+        Beep (494, 120)  #  Treble B
+        time.sleep(0.140)
+        Beep (330, 120)  #  Treble E
+        time.sleep(0.120)        
+        Beep (523, 120)  #  Treble C
+        time.sleep(0.120)
+        Beep (494, 120)  #  Treble B
+        time.sleep(0.140)
+        Beep (440, 120)  #  Treble A
+
+    def __TocarMusica4(self):
+        Beep (300, 500)
+        time.sleep(0.1) 
+        Beep (300, 500)
+        time.sleep(0.1)
+        Beep (300, 500)
+        time.sleep(0.10)
+        Beep (250, 500)
+        time.sleep(0.10)
+        Beep (350, 250)
+        Beep (300, 500)
+        time.sleep(0.10)
+        Beep (250, 500)
+        time.sleep(0.10)
+        Beep (350, 250)
+        Beep (300, 500)
+        time.sleep(0.10)
+
+    def __TocarMusica5(self):
+        Beep(658, 125) 
+        Beep(1320, 500) 
+        Beep(990, 250) 
+        Beep(1056, 250)
+        Beep(1188, 250)
+        Beep(1320, 125)
+        Beep(1188, 125)
+        Beep(1056, 250) 
+        Beep(990, 250) 
+        Beep(880, 500) 
+        Beep(880, 250) 
+        Beep(1056, 250) 
+        Beep(1320, 500) 
+        Beep(1188, 250) 
+        Beep(1056, 250) 
+        Beep(990, 750) 
+        Beep(1056, 250) 
+        Beep(1188, 500) 
+        Beep(1320, 500) 
+        Beep(1056, 500) 
+        Beep(880, 500) 
+        Beep(880, 500) 
+        time.sleep (0.250 )
+        Beep(1188, 500) 
+        Beep(1408, 250) 
+        Beep(1760, 500) 
+        Beep(1584, 250) 
+        Beep(1408, 250) 
+        Beep(1320, 750) 
+        Beep(1056, 250) 
+        Beep(1320, 500) 
+        Beep(1188, 250) 
+        Beep(1056, 250) 
+        Beep(990, 500) 
+        Beep(990, 250) 
+        Beep(1056, 250) 
+        Beep(1188, 500) 
+        Beep(1320, 500) 
+        Beep(1056, 500) 
+        Beep(880, 500) 
+        Beep(880, 500) 
+        time.sleep (0.500 )
+        Beep(1320, 500)
+
+        Beep(990, 250) 
+        Beep(1056, 250) 
+        Beep(1188, 250) 
+        Beep(1320, 125) 
+        Beep(1188, 125) 
+        Beep(1056, 250) 
+        Beep(990, 250) 
+        Beep(880, 500) 
+        Beep(880, 250) 
+        Beep(1056, 250) 
+        Beep(1320, 500) 
+        Beep(1188, 250) 
+        Beep(1056, 250) 
+        Beep(990, 750) 
+        Beep(1056, 250) 
+        Beep(1188, 500) 
+        Beep(1320, 500) 
+        Beep(1056, 500) 
+        Beep(880, 500) 
+        Beep(880, 500) 
+        time.sleep( 0.250 )
+        Beep(1188, 500)
+        Beep(1408, 250) 
+        Beep(1760, 500)   
+        Beep(1584, 250)    
+        Beep(1408, 250)     
+        Beep(1320, 750) 
+        Beep(1056, 250)
+        Beep(1320, 500) 
+        Beep(1188, 250) 
+        Beep(1056, 250) 
+        Beep(990, 500) 
+        Beep(990, 250) 
+        Beep(1056, 250) 
+        Beep(1188, 500) 
+        Beep(1320, 500) 
+        Beep(1056, 500) 
+        Beep(880, 500) 
+        Beep(880, 500) 
+        time.sleep (0.500)
+        Beep(660, 1000) 
+        Beep(528, 1000) 
+        Beep(594, 1000) 
+        Beep(495, 1000) 
+        Beep(528, 1000) 
+        Beep(440, 1000) 
+        Beep(419, 1000) 
+        Beep(495, 1000) 
+        Beep(660, 1000) 
+        Beep(528, 1000) 
+        Beep(594, 1000) 
+        Beep(495, 1000) 
+        Beep(528, 500) 
+        Beep(660, 500) 
+        Beep(880, 1000) 
+        Beep(838, 2000) 
+        Beep(660, 1000) 
+        Beep(528, 1000) 
+        Beep(594, 1000) 
+        Beep(495, 1000) 
+        Beep(528, 1000) 
+        Beep(440, 1000) 
+        Beep(419, 1000) 
+        Beep(495, 1000) 
+        Beep(660, 1000) 
+        Beep(528, 1000) 
+        Beep(594, 1000) 
+        Beep(495, 1000) 
+        Beep(528, 500) 
+        Beep(660, 500) 
+        Beep(880, 1000) 
+        Beep(838, 2000)
+
+#################----BASICOS----#########################
+
     def Encender(self,estado):
         if estado == True:
             print("Encendiendo Parlante")
@@ -377,15 +601,72 @@ class parlante():
                 print("Parlante encendido.\nBienvenido!.")
                 break
             self.isEncendido = True
-            self.__TocarMusica1()
+            self.__MusicEncendido()
             return 1
+    
+    def Apagar(self,estado):
+            print("")
+            print("Apagando Parlante")
+            for i in range (5):
+                time.sleep(0.5)
+                print("....")
+                print("Parlante Apagado.\nAdios.")
+                break
+            self.__MusicApagado()
 
-        else:
-            print("Parlante apagado")
+    def VerBateria(self,nivel):
+        print(self.bateria,"%")
+    
+    def VerVolumen(self):
+        print(self.volumen)
 
-ParlanteJBL = parlante("Rojo","JBL",True)
+    def PlayMusic(self):
+        try:
+            print("Reproduciendo audio:")
+            time.sleep(1)
+            self.__TocarMusica1()
+            time.sleep(2)
+            self.__TocarMusica2()
+            time.sleep(2)
+            self.__TocarMusica3()
+            time.sleep(2)
+            self.__TocarMusica4()
+            time.sleep(2)
+            self.__TocarMusica5()
+        except KeyboardInterrupt:
+            e = input("press enter:")
+    
+    def StopMusic(self,stop):
+        while PlayMusic == True:
+            break
 
+    def ChangeSong(self):
+        pass
+
+    def __Menu(self):
+        print("\nElige una opcion:\n1)Tocar cancion\n2)Detener\n3)Cambiar cancion\n4)Apagar Parlante")
+        
+        f = int(input("Cual Opcion eliges?:"))
+
+        while f > 0 and f< 5:
+            if f == 1:
+                self.PlayMusic()
+                break
+            elif f == 4:
+                print("jj")
+                self.Apagar(True)
+                break
+
+
+    def MostrarMenu(self):
+        self.__Menu()
+ParlanteJBL = parlante("Rojo","JBL",True,100,50)
 
 ParlanteJBL.Encender(True)
+ParlanteJBL.MostrarMenu()
+
+
+
+
 
 
